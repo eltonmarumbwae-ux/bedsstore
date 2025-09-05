f06c33acfc9e0ad74ebfd2e2b9b657ae022633be
eltonmarumbwae-ux <eltonmarumbwae@gmail.com>
2025-09-05 14:48:27 +0000

    Add files via upload
    initial commit

---
+++ footer.js
@@ -0,0 +1,7 @@
+document.getElementById('footer').innerHTML = `
+  <p>© 2025 BedStore | 
+    <a href="mailto:info@bedstore.co.za"><i class="fa-solid fa-envelope"></i>Email</a>
+    <a href="https://wa.me/27831234567" target="_blank"><i class="fa-brands fa-whatsapp"></i>WhatsApp</a>
+    <a href="tel:+27831234567"><i class="fa-solid fa-phone"></i>Phone</a>
+    <a href="https://facebook.com/bedstore" target="_blank"><i class="fa-brands fa-facebook"></i>Facebook</a>
+  </p>`;
